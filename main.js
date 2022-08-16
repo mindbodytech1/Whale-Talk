5d9e387f0dd60934e099a0fbfbab71240add5c47
mindbodytech1 <110260104+mindbodytech1@users.noreply.github.com>
2022-08-16 13:53:16 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,18 @@
+var input = 'turpentine and turtles';
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+var resultArray = [];
+for (i = 0; i < input.length; i++) {
+for (let j = 0; j < vowels.length; j++) {
+    if (input[i] === vowels[j]) {
+resultArray.push(input[i]);
+    }
+}
+if (input[i] === 'e');{
+    resultArray.push(input[i]);
+    }
+    if (input[i] === 'u');{
+        resultArray.push(input[i]);
+        }
+    };
+    
+    console.log(resultArray.join('').toUpperCase());
